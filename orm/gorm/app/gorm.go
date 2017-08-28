881330b44602f186d169d20d57eb3f788912479b
Mario Hros <spam@k3a.me>
2017-08-29 01:38:26 +0200

    orm modules directory and gorm controllers subdirectory with two types of controllers

--- orm/gorm/app/gorm.go
+++ orm/gorm/app/gorm.go
@@ -1,4 +1,4 @@
-package gorm
+package gormdb
 
 // # Database config
 // db.driver=sqlite3 # mysql, postgres, sqlite3
@@ -8,7 +8,6 @@ package gorm
 // db.password=dbpassword
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 
@@ -63,7 +62,7 @@ func InitDB() {
 	params := DbInfo{}
 	params.DbDriver = revel.Config.StringDefault("db.driver", "sqlite3")
 	params.DbHost = revel.Config.StringDefault("db.host", "localhost")
-	if params.DbDriver == "sqlite" && params.DbHost == "localhost" {
+	if params.DbDriver == "sqlite3" && params.DbHost == "localhost" {
 		params.DbHost = "/tmp/app.db"
 	}
 	params.DbUser = revel.Config.StringDefault("db.user", "default")
@@ -71,80 +70,4 @@ func InitDB() {
 	params.DbName = revel.Config.StringDefault("db.name", "default")
 
 	InitDBWithParameters(params)
-}
-
-// GormController controllers begin, commit and rollback transactions
-type GormController struct {
-	revel.Controller
-	Txn *gorm.DB
-}
-
-func (c *GormController) InitDB(params DbInfo) {
-	dbInfo := ""
-	switch params.DbDriver {
-	default:
-		dbInfo = fmt.Sprintf(params.DbHost)
-	case "postgres":
-		dbInfo = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", params.DbHost, params.DbUser, params.DbName, params.DbPassword)
-	case "mysql":
-		dbInfo = fmt.Sprintf("%s:%s@%s/%s?charset=utf8&parseTime=True&loc=Local", params.DbUser, params.DbPassword, params.DbHost, params.DbName)
-	}
-	OpenDB(params.DbDriver, dbInfo)
-
-}
-
-// Begin GormController to connect db
-func (c *GormController) Begin() revel.Result {
-
-	txn := DB.Begin()
-	if txn.Error != nil {
-		fmt.Println(c.Txn.Error)
-		panic(txn.Error)
-	}
-
-	c.Txn = txn
-	return nil
-}
-
-// Commit database transaction
-func (c *GormController) Commit() revel.Result {
-	if c.Txn == nil {
-		return nil
-	}
-
-	c.Txn.Commit()
-	if c.Txn.Error != nil && c.Txn.Error != sql.ErrTxDone {
-		fmt.Println(c.Txn.Error)
-		panic(c.Txn.Error)
-	}
-
-	c.Txn = nil
-	return nil
-}
-
-// Rollback transaction
-func (c *GormController) Rollback() revel.Result {
-	if c.Txn == nil {
-		return nil
-	}
-
-	c.Txn.Rollback()
-	if c.Txn.Error != nil && c.Txn.Error != sql.ErrTxDone {
-		fmt.Println(c.Txn.Error)
-		panic(c.Txn.Error)
-	}
-
-	c.Txn = nil
-	return nil
-}
-
-func init() {
-	revel.OnAppStart(func() {
-		if revel.Config.BoolDefault("db.autoinit", false) {
-			InitDB()
-			revel.InterceptMethod((*GormController).Begin, revel.BEFORE)
-			revel.InterceptMethod((*GormController).Commit, revel.AFTER)
-			revel.InterceptMethod((*GormController).Rollback, revel.FINALLY)
-		}
-	})
 }
